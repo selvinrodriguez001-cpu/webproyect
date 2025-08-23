d1e6f82f2a8db48cd413ae6b6e7e1f7b7dd4387f
selvinrodriguez001-cpu <selvinrodriguez001@gmail.com>
2025-08-23 16:51:56 -0600

    Add files via upload

---
+++ operaciones.js
@@ -0,0 +1,44 @@
+//Declarando variables de diferente tipos de daTOS
+
+//Numero con var
+var numero = 25;
+
+//Cadena de texto con let
+let saludo = "Hola, mundo";
+
+//Booleano con cost
+const esEstudiante = true;
+
+//Mostramos las variables en consola
+console.log("Numero:", numero);
+console.log("Texto:", saludo);
+console.log("Es estudiante?:", esEstudiante);
+
+
+//Mostrar las varibles en consola
+console.log("Numero:", numero1);
+console.log("Mensaje:", mensaje);
+console.log("Es valido?:", esValido);
+
+//Realizar operaciones aritmetricas
+let numero2 = 5;
+
+//Suma
+let Suma = numero1 + numero2;
+console.log("Suma:", suma);
+
+//Resta
+let Resta = numero1 - numero2;
+console.log("Resta:", resta);
+
+//Multiplicacion
+let Multiplicacion = numero1 * numero2;
+console.log("Multiplicacion:", multiplicacion);
+
+//Division
+let Division = numero1 / numero2;
+console.log("Division:", division);
+
+//Modulo (residuo)
+let modulo = numero1 % numero2;
+console.log("Modulo:", modulo);
